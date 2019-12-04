76e126f7d691340242b202eacfab40ad17f9ef3c
dawnlunacy <rudd.lacy@gmail.com>
2019-12-04 14:40:43 -0700

    Add export to App

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
+const express = require('express');
+const app = express();
+
 const cors = require('cors');
 app.use(cors());
 
-const express = require('express');
-const app = express();
 app.use(express.json());
 
 app.locals.title = "Welcome to Tone Zone api. Get yo' color on."
@@ -10,3 +11,5 @@ app.locals.title = "Welcome to Tone Zone api. Get yo' color on."
 app.get('/', (request, response ) => {
   response.send(app.locals.title)
 })
+
+export default app;
