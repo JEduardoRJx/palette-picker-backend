3ca407cbad24845920ddfc879c35cabb7c8d9252
dawnlunacy <rudd.lacy@gmail.com>
2019-12-04 14:49:19 -0700

    Add app.test.js

---
+++ app.test.js
@@ -0,0 +1,7 @@
+import "@babel/polyfill";
+
+
+
+const environment = process.env.NODE_ENV || 'development'
+const configuration = require('./knexfile')[environment]
+const database = require('knex')(configuration)
