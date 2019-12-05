865f6c2a598119443149bfd5f4fd670440ebcbdc
Eduardo <jeduardorjx@gmail.com>
2019-12-04 22:02:15 -0700

    Setup schema, migrations, and seeding

---
+++ db/migrations/20191204203601_initial.js
@@ -0,0 +1,38 @@
+
+exports.up = (knex) => {
+  return Promise.all([
+    knex.schema.createTable('users', (table) => {
+      table.increments('id').primary();
+      table.string('username').notNullable();
+      table.string('password').notNullable();
+      table.timestamps(true, true);
+    }),
+    knex.schema.createTable('projects', (table) => {
+      table.increments('id').primary();
+      table.string('project_name').notNullable();
+      table.integer('user_id').unsigned();
+      table.foreign('user_id').references('users.id');
+      table.timestamps(true, true);
+    }),
+    knex.schema.createTable('palettes', (table) => {
+      table.increments('id').primary();
+      table.string('palette_name').notNullable();
+      table.integer('project_id').unsigned();
+      table.foreign('project_id').references('projects.id');
+      table.string('color1').notNullable();
+      table.string('color2').notNullable();
+      table.string('color3').notNullable();
+      table.string('color4').notNullable();
+      table.string('color5').notNullable();
+      
+      table.timestamps(true, true);
+    })
+  ]);
+};
+exports.down = (knex) => {
+  return Promise.all([
+    knex.schema.dropTable('palettes'),
+    knex.schema.dropTable('projects'),
+    knex.schema.dropTable('users')
+  ]);
+};
---
+++ db/seeds/dev/users.js
@@ -0,0 +1,65 @@
+const data = require('../../../data');
+const users = data.users;
+const projects = data.projects;
+const palettes = data.palettes;
+
+const createUser = (knex, user) => {
+  return knex('users').insert({
+    username: user.username,
+    password: user.password
+  });
+}
+
+const createProject = (knex, project) => {
+  let userID = knex.from('users')
+    .select('id')
+    .where('id', project.user_id)
+  return knex('projects').insert({
+    project_name: project.project_name,
+    user_id: userID
+  })
+}
+
+const createPalette = (knex, palette) => {
+  let projectID = knex.from('projects')
+    .select('id')
+    .where('id', palette.project_id)
+  return knex('palettes').insert({
+    palette_name: palette.palette_name,
+    project_id: projectID,
+    color1: palette.color1,
+    color2: palette.color2,
+    color3: palette.color3,
+    color4: palette.color4,
+    color5: palette.color5
+  })
+}
+
+exports.seed = (knex) => {
+  // Deletes ALL existing entries
+  return knex('palettes').del()
+    .then(() => knex('projects').del())
+    .then(() => knex('users').del())
+    .then(() => {
+      let userPromises = [];
+      users.forEach(user => {
+        userPromises.push(createUser(knex, user));
+      });
+      return Promise.all(userPromises);
+    })
+    .then(() => {
+      let projectPromises = [];
+      projects.forEach(project => {
+        projectPromises.push(createProject(knex, project));
+      });
+      return Promise.all(projectPromises);
+    })
+    .then(() => {
+      let palettePromises = [];
+      palettes.forEach(palette => {
+        palettePromises.push(createPalette(knex, palette));
+      });
+      return Promise.all(palettePromises);
+    })
+    .catch(error => console.log(`Error seeding data ${error}`))
+};
