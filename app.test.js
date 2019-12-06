2f5fc41b5bcb35a56b84fd9b87123b94380bf11c
Lacy Rudd <rudd.lacy@gmail.com>
2019-12-06 10:34:22 -0700

    Tdd database (#21)
    * Install babel/polyfill and write test for GETting a specific user's projects (all) and for getting a specific user's specific project
    
    * Add GET test for fetching all of a specific users palettes and one of a specific users palettes
    
    * Add async for tests and write SAD path for GET test for getting all of a user's projects and SAD path for GET test for getting a user's specific project
    
    * Add SAD path for getting a projects specific palettes and one specific palette
    
    * Add interpolation for /api/v1/:user_id/projects/:project_id/palettes test HAPPY path
    
    * Add should statement for /api/v1/:user_id/projects/:project_id/palettes/:palette_id get test
    
    * Move async from describe block to it block

--- app.test.js
+++ app.test.js
@@ -5,3 +5,112 @@ import app from './app';
 const environment = process.env.NODE_ENV || 'development'
 const configuration = require('./knexfile')[environment]
 const database = require('knex')(configuration)
+
+describe('Server', () => {
+  beforeEach(async () => {
+    await database.seed.run()
+  })
+
+  describe('init', () => {
+    it('should return a 200 status', async () => {
+      const res = await request(app).get('/')
+      expect(res.status).toBe(200)
+    });
+  });
+
+  describe('GET /api/v1/:user_id/projects', () => {
+    it('should return 200 status code and all of the specified users projects', async () => {
+      //Setup
+      const expectedProjects = await database('projects').first();
+      console.log(expectedProjects)
+      //Execution
+      const response = await request(app).get('/api/v1/1/projects');
+      const projects = response.body;
+
+      //Expectation
+      expect(response.status).toBe(200);
+      expect(projects).toEqual(expectedProjects);
+    });
+
+    it('should return a 404 and the message "User not found" ', async () => {
+      const invalidID = -1;
+
+      const response = await request(app).get(`/api/v1/${invalidID}/projects`);
+
+      expect(response.status).toBe(404);
+      expect(response.body.error).toEqual('User not found');
+    })
+  });
+
+  describe('GET /api/v1/:user_id/projects/:project_id', () => {
+    it('should return a 200 status code and all of the specified users specific project', async () => {
+      //Setup
+      const expectedProject = await database('projects').first();
+      const { id } = expectedProject;
+      //Exection
+      const response = await request(app).get(`/api/v1/${id}/projects/1`);
+      const project = response.body
+      //Expectation
+      expect(response.status).toBe(200);
+      expect(project).toEqual(expectedProject);
+    });
+
+    it('should return a 404 and the message "Project not found" ', async () => {
+      const invalidID = -1;
+
+      const response = await request(app).get(`/api/v1/:user_id/projects/${invalidID}`);
+
+      expect(response.status).toBe(404);
+      expect(response.body.error).toEqual('Project not found');
+    });
+  });
+
+  describe('GET /api/v1/:user_id/projects/:project_id/palettes',  () => {
+    it('should return a status code of 200 and return all palettes from a specified users specific project', async () => {
+      //Setup
+    const expectedPalettes = await database('palettes').first();
+    const { id } = expectedPalettes;
+    //Execution
+    const response = await request(app).get(`/api/v1/1/projects/${id}/palettes`);
+    const palettes = response.body;
+
+    //Expectation
+    expect(response.status).toBe(200);
+    expect(palettes).toEqual(expectedPalettes)
+    });
+
+    it('should return a 404 and the message "Project not found, no palettes to return" ', async () => {
+      const invalidID = -1;
+
+      const response = await request(app).get(`/api/v1/:user_id/projects/${invalidID}/palettes`);
+
+      expect(response.status).toBe(404);
+      expect(response.body.error).toEqual('Project not found, no palettes to return');
+    });
+  });
+
+  describe('GET /api/v1/:user_id/projects/:project_id/palettes/:palette_id',  () => {
+    it('should return a status code of 200 and the specific palette requested', async () => {
+      //Setup
+    const expectedPalette = await database('palettes').first();
+    const { id } = expectedPalette;
+
+    //Expectation
+    const response = await response(app).get(`/api/v1/:user_id/projects/:project_id/palettes/${id}`);
+    const palette = response.body;
+
+    //Execution
+    expect(response.status).toBe(200);
+    expect(palette).toEqual(expectedPalette);
+    });
+  });
+
+  it('should return a 404 and the message "Palette not found" ', async () => {
+    const invalidID = -1;
+
+    const response = await request(app).get(`/api/v1/:user_id/projects/:project_id/palettes/${invalidID}`);
+
+    expect(response.status).toBe(404);
+    expect(response.body.error).toEqual('Palette not found');
+  });
+});
