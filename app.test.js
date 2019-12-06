cb49d8963e3fd6a2c74d5df8f86242541da82223
dawnlunacy <rudd.lacy@gmail.com>
2019-12-05 21:55:07 -0700

    Install babel/polyfill and write test for GETting a specific user's projects (all) and for getting a specific user's specific project

--- app.test.js
+++ app.test.js
@@ -5,3 +5,45 @@ import app from './app';
 const environment = process.env.NODE_ENV || 'development'
 const configuration = require('./knexfile')[environment]
 const database = require('knex')(configuration)
+
+describe('Server', () => {
+  beforeEach(async () => {
+    await database.seed.run()
+  })
+
+  describe('init', () => {
+    it('should return a 200 status', async () => {
+      const res = await request(app).get('/')
+      expect(res.status).toBe(200)
+    });
+  });
+
+  describe('GET /api/v1/:user_id/projects', () => {
+    it('should return 200 status code and all of the specified users projects', async() => {
+      //Setup
+      const expectedProjects = await database('projects').first();
+      console.log(expectedProjects)
+      //Execution
+      const response = await request(app).get('/api/v1/1/projects');
+      const projects = response.body;
+
+      //Expectation
+      expect(response.status).toBe(200);
+      expect(projects).toEqual(expectedProjects);
+    })
+  })
+
+  describe('GET /api/v1/:user_id/projects/:project_id', () => {
+    it('should return a 200 status code and all of the specified users specific project', () => {
+      //Setup
+      const expectedProject = await database('projects').first();
+
+      //Exection
+      const response = await request(app).get('/api/v1/1/projects/1');
+      const project = response.body
+      //Expectation
+      expect(response.state).toBe(200);
+      expect(project).toEqual(expectedProject);
+    })
+  })
+});
